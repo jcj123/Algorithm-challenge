39ebd0d820378ed383ee59f107e94ad34562cf85
lyx <326406750@qq.com>
2017-12-01 13:11:03 +0800

    修改

--- Algorithm/src/sf/sgs/Algorithm/FirstCode.java
+++ Algorithm/src/sf/sgs/Algorithm/FirstCode.java
@@ -27,6 +27,7 @@ public class FirstCode {
         TestBean testBean = getEnvironment();
         mId = testBean.getId();
         mStep = 0;
+
         algorithm(testBean);
         getReplay(testBean.getId());
     }
@@ -36,8 +37,8 @@ public class FirstCode {
         List<TestBean.StateBean.JobsBean> jobs = testBean.getState().getJobs();
         List<TestBean.StateBean.WallsBean> walls = testBean.getState().getWalls();
 
-        Point pointNow = new Point(0, 0);
-        pointNow.distance = 0;
+        mPointStart = new Point(0, 0);
+        mPointStart.distance = 0;
 
         //初始化网格
         for (int i = 0; i < 12; i++) {
@@ -48,7 +49,7 @@ public class FirstCode {
         }
 
         for (TestBean.StateBean.JobsBean job : jobs) {
-            value[job.getY()][job.getX()] = job.getValue();
+            allPoint[job.getY()][job.getX()].value = job.getValue();
         }
 
         for (TestBean.StateBean.WallsBean wall : walls) {
@@ -58,33 +59,34 @@ public class FirstCode {
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 12; j++) {
                 //左
-                if (i >= 1 && i <= 12 && !allPoint[i - 1][j].fobidden) {
+                if (i >= 1  && !allPoint[i - 1][j].fobidden) {
                     allPoint[i][j].nextPointList.add(allPoint[i - 1][j]);
                 }
                 //右
-                if (i >= 0 && i <= 11 && !allPoint[i - 1][j].fobidden) {
+                if (i <= 10 && !allPoint[i + 1][j].fobidden) {
                     allPoint[i][j].nextPointList.add(allPoint[i + 1][j]);
                 }
                 //上
-                if (j >= 1 && i <= 12 && !allPoint[i][j - 1].fobidden) {
+                if (j >= 1  && !allPoint[i][j - 1].fobidden) {
                     allPoint[i][j].nextPointList.add(allPoint[i][j - 1]);
                 }
                 //下
-                if (j >= 0 && i <= 11 && !allPoint[i][j + 1].fobidden) {
+                if ( j <= 10 && !allPoint[i][j + 1].fobidden) {
                     allPoint[i][j].nextPointList.add(allPoint[i][j + 1]);
                 }
             }
         }
 
         //模型初始化完成
-        findPath(pointNow);
-//        pointNow =
 
+        while(mStep < 288){
+            findPath(mPointStart);
+        }
     }
 
 
-    private static void findPath(Point pointNow){
-        pointNow.distance = 0;
+    private static void findPath(Point pointStart){
+        pointStart.distance = 0;
 
         Set<Point> viewedPointSet = new HashSet<Point>();
         Set<Point> unViewedPointSet = new HashSet<Point>();
@@ -93,7 +95,7 @@ public class FirstCode {
                 unViewedPointSet.add(allPoint[i][j]);
             }
         }
-        viewedPointSet.add(pointNow);
+        viewedPointSet.add(pointStart);
 
 
         //核心算法
@@ -117,7 +119,7 @@ public class FirstCode {
 
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 12; j++) {
-                value[i][j] = value[i][j] - allPoint[i][j].distance;
+                allPoint[i][j].value = allPoint[i][j].value - allPoint[i][j].distance;
 //                xingJiaBi[i][j] = value[i][j]/allPoint[i][j].distance;
             }
         }
@@ -128,8 +130,8 @@ public class FirstCode {
         int tempValue = Integer.MIN_VALUE;
         for(int i=0; i<12; i++){
             for(int j = 0; j<12; j++){
-                if(value[i][j] > tempValue){
-                    tempValue = value[i][j];
+                if(allPoint[i][j].value > tempValue){
+                    tempValue = allPoint[i][j].value;
                     destPointY = i;
                     destPointX = j;
                 }
@@ -138,9 +140,9 @@ public class FirstCode {
 
         //找到目标节点，并返回路径
         Point destPoint = allPoint[destPointX][destPointY];
-        goToTheDestPoint(pointNow,destPoint);
+        goToTheDestPoint(pointStart,destPoint);
         resetAllPointInfo();
-        mPointNow = destPoint;
+        mPointStart = destPoint;
     }
 
 
@@ -177,16 +179,18 @@ public class FirstCode {
     //重置节点数据
     private static void resetAllPointInfo(){
         List<ResultBean.StateBean.JobsBean> jobs = mResultBean.getState().getJobs();
-        for (ResultBean.StateBean.JobsBean job : jobs) {
-            value[job.getY()][job.getX()] = job.getValue();
-        }
 
         for(int i=0; i<12; i++){
             for(int j = 0; j<12; j++){
                 allPoint[i][j].hasAdd = false;
                 allPoint[i][j].previousPoint = null;
+                allPoint[i][j].value = 0;
                 allPoint[i][j].distance = Integer.MAX_VALUE;
             }
+        }
+
+        for (ResultBean.StateBean.JobsBean job : jobs) {
+            allPoint[job.getY()][job.getX()].value = job.getValue();
         }
     }
 
@@ -224,6 +228,7 @@ public class FirstCode {
         int x;
         int y;
         int distance;
+        int value;
         boolean fobidden;
         boolean hasAdd;
         HashSet<Point> nextPointList;
@@ -236,8 +241,8 @@ public class FirstCode {
             distance = Integer.MAX_VALUE;
         }
 
-        public Point(int value) {
-            this.distance = value;
+        public Point(int distance) {
+            this.distance = distance;
         }
     }
 }
